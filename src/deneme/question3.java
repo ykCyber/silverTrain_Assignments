203c05357c3f36583e1ea841bc74188a5011d58e
YusufKenan <59359546+ykCyber@users.noreply.github.com>
2020-01-31 21:56:00 +0300

    Update question3.java

--- src/deneme/question3.java
+++ src/deneme/question3.java
@@ -5,6 +5,7 @@ public class question3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Acerden yazýlý");
+		System.out.println("Asustan yazýldý");
 	}
 
 }
