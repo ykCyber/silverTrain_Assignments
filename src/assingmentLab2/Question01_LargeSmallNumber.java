f1cf620bbfba5cf3f70226a04949a884af347fcd
ykCyber <59359546+ykCyber@users.noreply.github.com>
2020-01-31 22:24:23 +0300

    Acer

--- src/assingmentLab2/Question01_LargeSmallNumber.java
+++ src/assingmentLab2/Question01_LargeSmallNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question01_LargeSmallNumber {
 
 	public static void main(String[] args) {
-		largeSmallnumberDefiner();
+		largeSmallnumberDefiner();	
 	}
 
 	public static void largeSmallnumberDefiner() {
