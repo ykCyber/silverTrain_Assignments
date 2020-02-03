50d944514a787850d268125360085ffe8393a822
ykCyber <59359546+ykCyber@users.noreply.github.com>
2020-02-03 23:28:18 +0300

    I add an new method to check palindrome. I used StringBuffer reverse method.

--- src/lab3Cont/Question17isPalindrome.java
+++ src/lab3Cont/Question17isPalindrome.java
@@ -2,14 +2,24 @@ package lab3Cont;
 
 public class Question17isPalindrome {
 
-	public static void main(String[] args ) {
-		String str= "Able was I ere I saw Elba";
-		isPalindrome( str);
+	public static void main(String[] args) {
+	
+		String str = "Able was I ere I saw Elba";
+		isPalindrome(str);
+		
+		System.out.println(checkPalindrome("kaMasasaMaK"));
+	}
+
+	public static boolean checkPalindrome(String str) {
+		StringBuffer sbf = new StringBuffer(str);
+		
+		return str.equalsIgnoreCase(sbf.reverse().toString());
+
 	}
 
 	public static boolean isPalindrome(String str) {
-		StringBuilder sb =new StringBuilder("");
-		for (int i = str.length()-1; i >=0 ; i--) {
+		StringBuilder sb = new StringBuilder("");
+		for (int i = str.length() - 1; i >= 0; i--) {
 			sb.append(str.charAt(i));
 		}
 		System.out.println(sb.toString().equalsIgnoreCase(str));
