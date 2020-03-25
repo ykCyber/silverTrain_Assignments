f33c4a326f786ae364dbcf5fcf7bc400fe41ac4c
ykCyber <59359546+ykCyber@users.noreply.github.com>
2020-03-25 17:32:23 +0300

    StringBuffer,RandomNumber charAt dash,

--- src/WordGame/main.java
+++ src/WordGame/main.java
@@ -7,10 +7,34 @@ public class main {
 		for (int i = 0; i < 5; i++) {
 			howManyLetters();	
 			printQuestionNumber();
-			printDashes();
+			printDashes(); 
+			askUserHint();
+			StringBuffer sb = new StringBuffer();
+			sb.setCharAt(rn, anwer[6].chartAt(rn));
+			/*question[6]=
+		    answer[6]= corona;*/
+			String userAnswer = "-".repeat(anwer[6].length());//------
+			rn = rn(anwer[6].lenght) 0;/0 1 2 3 4 5
+			if(userAnswer.charAt(rn)=="-" ) {
+				
+				userAnswer.rep
+			String alfa = ""+answer[6].chartAt(rn);
 			
+			//for (i=0, ,i<answer[6]lengt,i++) {
+				String answerUser= answerUser+"-";
+				if (i == rn) {
+					answerUser= asnwerUser+alfa;
+				}
+				}
+			}
+				System.out.println(anserUser);
 			
 		}
+	}
+
+	private static void askUserHint() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void printDashes() {
