e9320d8d94bd4ebef7acf1e19bd602ccfdacdf93
ykCyber <yusufkenan2604@gmail.com>
2020-04-20 01:18:36 +0300

    a14 q09

--- src/assignment14/Question09/Telephone.java
+++ src/assignment14/Question09/Telephone.java
@@ -1,5 +1,15 @@
 package assignment14.Question09;
-
+//manası çok derin
 public class Telephone {
+	private String calledNumber;
+	public static int quantity = 250;
+	public static double total = 15658.92d;
 
+	public String getNumber() {
+		return calledNumber;
+	}
+
+	public void setNumber(String calledNumber) {
+		this.calledNumber = calledNumber;
+	}
 }
