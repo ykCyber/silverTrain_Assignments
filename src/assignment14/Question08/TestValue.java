440014c0ed7783fe063af966eeeea05ada40b4da
ykCyber <yusufkenan2604@gmail.com>
2020-04-20 01:00:21 +0300

    Assngmnt 14 Q 08

--- src/assignment14/Question08/TestValue.java
+++ src/assignment14/Question08/TestValue.java
@@ -3,7 +3,6 @@ package assignment14.Question08;
 public class TestValue {
 
 	public static void main(String[] args) {
-		Value.setVal(0);
 		System.out.println(Value.wasModified());
 		Value value = new Value();
 		System.out.println(Value.wasModified());
